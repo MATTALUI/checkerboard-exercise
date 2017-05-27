f07ca89bf8468dbcaea5c624a6f6a3c511c7b9f2
Matthew Hummer <biggreyson@gmail.com>
2017-05-26 19:46:42 -0600

    got some of the basics down

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Your JS goes here
+const body = document.getElementsByTagName('body')[0];
+
+for (let i = 0; i< 8; i++){
+  let newDiv = document.createElement('div');
+  newDiv.style.width = "1vh";
+  newDiv.style.height = '1vh';
+  if (i % 2){
+    newDiv.style.backgroundColor = 'red';
+  }else{
+    newDiv.style.backgroundColor = 'black';
+  }
+  body.appendChild(newDiv);
+}
